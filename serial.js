01d50b58c26b1c3dfc527cdc2411792c07b26d55
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2015-07-18 15:16:46 +0200

    style(serial): Cleaning as of jshint

--- serial.js
+++ serial.js
@@ -1,26 +1,31 @@
 var serial_maker = function () {
-     // Produce an object that produces unique strings. A
-     // unique string is made up of two parts: a prefix
-     // and a sequence number. The object comes with
-     // methods for setting the prefix and sequence
-￼Module | 41
-// number, and a gensym method that produces unique
-// strings.
-    var prefix = '';
-    var seq = 0;
-    return {
-        set_prefix: function (p) {
-            prefix = String(p);
-        },
-        set_seq: function (s) {
-seq = s; },
-gensym: function () {
-var result = prefix + seq; seq += 1;
-return result;
-} };
+  // Produce an object that produces unique strings. A
+  // unique string is made up of two parts: a prefix
+  // and a sequence number. The object comes with
+  // methods for setting the prefix and sequence
+  // number, and a gensym method that produces unique
+  // strings.
+  var prefix = '';
+  var seq = 0;
+  return {
+    set_prefix: function (p) {
+      prefix = String(p);
+    },
+    set_seq: function (s) {
+      seq = s;
+    },
+    gensym: function () {
+      var result = prefix + seq;
+      seq += 1;
+      return result;
+    }
+  };
 };
-var seqer =
-seqer.set_prefix = ('Q';) seqer.set_seq = (1000);
-var unique = seqer.gensym();
+var seqer = serial_maker();
+seqer.set_prefix = ('Q');
+seqer.set_seq = (1000);
 serial_maker();
+var unique = seqer.gensym();
+var k2 = 'test';
+
 // unique is "Q1000"
