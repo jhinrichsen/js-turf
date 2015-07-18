dd78a46da33f8183f9a86a5c951ddff7213c697d
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2015-07-18 15:23:50 +0200

    feat(counter): ES6 implementation of a counter

---
+++ counter.js
@@ -0,0 +1,58 @@
+/*jshint node: true*/
+
+"use strict";
+var assert = require('assert');
+
+var i = 1;
+
+
+var counter = function () {
+  var counter = 0;
+  return {
+    inc: function () {
+      counter = counter + 1;
+      return counter;
+    },
+    dec: function () {
+      counter = counter - 1;
+      return counter;
+    },
+    get: function () {
+      return counter;
+    },
+    reset: function () {
+      counter = 0;
+      return counter;
+    }
+  };
+};
+
+assert(counter().get() === 0);
+assert(counter().inc() === 1);
+assert(counter().dec() === -1);
+
+var builderCounter = function () {
+  var counter = 0;
+  return {
+    inc: function () {
+      counter = counter + 1;
+      return this;
+    },
+    dec: function () {
+      counter = counter - 1;
+      return this;
+    },
+    get: function () {
+      return counter;
+    },
+    reset: function () {
+      counter = 0;
+      return this;
+    }
+  };
+};
+
+assert(builderCounter().get() === 0);
+assert(builderCounter().inc().inc().get() === 2);
+assert(builderCounter().inc().inc().dec().dec().get() === 0);
+assert(builderCounter().inc().reset().get() === 0);
